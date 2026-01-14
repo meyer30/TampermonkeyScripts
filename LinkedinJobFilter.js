d9e1281ea4a12a204b20a92de33326e7a5c72db4
meyer30 <2603221+meyer30@users.noreply.github.com>
2026-01-14 15:26:18 -0600

    Update LinkedinJobFilter.js

--- LinkedinJobFilter.js
+++ LinkedinJobFilter.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Linkedin Search Filter
 // @namespace    http://tampermonkey.net/
-// @version      2026-01-12
-// @description  Block job listings from certain companies from showing up.  Update the BLOCKED_COMPANIES List to use for your results
+// @version      1.0.1
+// @description  Block job listings from certain companies from showing up
 // @author       Joel Meyer
-// @match        https://www.linkedin.com/jobs/search-results/*
+// @match        https://www.linkedin.com/jobs/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=linkedin.com
 // @grant        none
 // ==/UserScript==
@@ -13,23 +13,47 @@
     'use strict';
 
     const BLOCKED_COMPANIES = [
-        'Acme Transport',
-        'Acme Oil',
+        'ACME OIL',
+        'ACME RUBBER',
     ];
 
-   function removeBlockedJobs(root = document) {
-  root.querySelectorAll('p').forEach(p => {
-    const text = p.textContent;
+    const DISMISSED_TEXT = "We won’t show you this job again";
 
-    if (BLOCKED_COMPANIES.some(name => text.includes(name))) {
-      const card = p.closest('[data-view-name="job-search-job-card"]');  //Linkedin may change this but works as of 01/14/2026.
-      if (card) card.remove();
+    function removeBlockedJobs(root = document) {
+       root.querySelectorAll('p').forEach(p => {
+           const text = p.textContent;
+
+           if (BLOCKED_COMPANIES.some(name => text.includes(name))) {
+               const card = p.closest('[data-view-name="job-search-job-card"]');  //Linkedin regular search
+               if (card) card.remove();
+           }
+       });
+
+       root.querySelectorAll('.artdeco-entity-lockup__subtitle span')  //Linkedin Recommend jobs
+           .forEach(span => {
+           const company = span.textContent.trim();
+
+           if (BLOCKED_COMPANIES.includes(company))
+               span.closest('.job-card-container')?.remove();
+
+       });
+   }
+
+    function removeDismissedJobs(root = document) {
+        root.querySelectorAll('*').forEach(el => {
+            if (el.textContent?.includes(DISMISSED_TEXT)) {
+                const card = el.closest('.job-card-container');
+                if (card) {
+                    card.remove();
+                }
+            }
+        });
     }
-  });
-}
+
 
 // Initial cleanup
 removeBlockedJobs();
+removeDismissedJobs();
 
 // Watch for new jobs being added
 const observer = new MutationObserver(mutations => {
@@ -37,6 +61,7 @@ const observer = new MutationObserver(mutations => {
     m.addedNodes.forEach(node => {
       if (node.nodeType === 1) {
         removeBlockedJobs(node);
+        removeDismissedJobs(node);
       }
     });
   });
